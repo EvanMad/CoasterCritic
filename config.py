391d82911877ed42510c6ecc15ee348b48e665b6
EvanM <evan.madurai@gmail.com>
2023-11-26 19:32:56 +0000

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-WTF_CSRF_ENABLED = True
-SECRET_KEY = '\x8dO\xedb\xbf\xd2\xab\xb9\xfe\n\xb9I\xcbY\xa5\x18im!?\x8e\xafj\xae'
-
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = True
